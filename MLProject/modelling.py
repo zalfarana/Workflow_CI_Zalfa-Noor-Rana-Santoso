9cdfeda2f053d86f95bb02df4b4b474c6b738982
zalfarana <zzalfa302@gmail.com>
2025-12-16 14:35:29 +0700

    Update conda.yaml to configure environment for MLflow with updated dependencies

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,30 +1,28 @@
-import argparse
 import pandas as pd
 import mlflow
 import mlflow.sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--max_iter", type=int, default=1000)
-args = parser.parse_args()
+def main():
+    print("Loading data...")
 
-# Load data
-X_train = pd.read_csv("titanic_preprocessing/X_train_processed.csv")
-X_test  = pd.read_csv("titanic_preprocessing/X_test_processed.csv")
-y_train = pd.read_csv("titanic_preprocessing/y_train.csv").squeeze()
-y_test  = pd.read_csv("titanic_preprocessing/y_test.csv").squeeze()
+    X_train = pd.read_csv("titanic_preprocessing/X_train_processed.csv")
+    X_test  = pd.read_csv("titanic_preprocessing/X_test_processed.csv")
+    y_train = pd.read_csv("titanic_preprocessing/y_train.csv").squeeze()
+    y_test  = pd.read_csv("titanic_preprocessing/y_test.csv").squeeze()
 
+    mlflow.sklearn.autolog()
 
-mlflow.sklearn.autolog()
+    with mlflow.start_run():
+        model = LogisticRegression(max_iter=1000)
+        model.fit(X_train, y_train)
 
-with mlflow.start_run():
-    model = LogisticRegression(max_iter=args.max_iter)
-    model.fit(X_train, y_train)
+        preds = model.predict(X_test)
+        acc = accuracy_score(y_test, preds)
 
-    preds = model.predict(X_test)
-    acc = accuracy_score(y_test, preds)
+        mlflow.log_metric("accuracy", acc)
+        print("Accuracy:", acc)
 
-    mlflow.log_metric("accuracy", acc)
-
-    print("Training finished. Accuracy:", acc)
+if __name__ == "__main__":
+    main()
